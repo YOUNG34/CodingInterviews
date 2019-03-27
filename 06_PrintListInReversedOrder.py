978f5b4040fbc2ab926254989e7a9f070961a74e
YOUNG34 <32638115+YOUNG34@users.noreply.github.com>
2019-03-27 18:10:16 +0800

    Update 06_PrintListInReversedOrder.py

--- 06_PrintListInReversedOrder.py
+++ 06_PrintListInReversedOrder.py
@@ -1,1 +1,33 @@
+'''
+/*******************************************************************
+Copyright(c) 2016, Harry He
+All rights reserved.
+Distributed under the BSD license.
+(See accompanying file LICENSE.txt at
+https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
+*******************************************************************/
 
+//==================================================================
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者：何海涛
+//==================================================================
+
+// 面试题6：从尾到头打印链表
+// 题目：输入一个链表的头结点，从尾到头反过来打印出每个结点的值。
+'''
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        # write code here
+        target = list()
+        head = listNode
+        while head:
+            target.insert(0,head.val)
+            head = head.next
+        return(target)
